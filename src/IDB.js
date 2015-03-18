bb4321dcd679d3072b09beea59f7a744f40c6d8f
Lucas Calje <lucas@frontmen.nl>
2015-03-18 18:43:43 +0100

    Merge branch 'master' into dev
    Conflicts:
    	idb.js

--- src/IDB.js
+++ src/IDB.js
@@ -2,9 +2,8 @@
 
 //import {TestInterface} from './TestInterace.js';
 //const VERSION = '1.2';
-//constructor(options={dbname: 'idb-test', store: 'customers'}) {
 export default class IDB {
-  constructor(options) {
+  constructor(options={dbname: 'idb-test', store: 'customers'}) {
     //this._idb = getIndexedDBObject('indexedDB');
   }
 }
@@ -21,7 +20,7 @@ function getIndexedDBObject(name) {
 	var DBNAME = 'idb-test', OBJECTSTORENAME = 'customers', VERSION = 12 ;
 	var idb, dbconn ;
 	var disabled = false ;
-	
+
 	/*
 		options:
 			* callback - a callback function which expectes: {setup:true} (succes) or {setup:false} (error) object
@@ -61,9 +60,9 @@ function getIndexedDBObject(name) {
 				var transaction = getTransaction('readonly') ;
 				if ( transaction ) {
 					var objectStore = transaction.objectStore(OBJECTSTORENAME) ;
-					var request = objectStore.get(options.record[options.columnName]) ; 
-                                
-					request.onsuccess = function(e) {                                        
+					var request = objectStore.get(options.record[options.columnName]) ;
+
+					request.onsuccess = function(e) {
 						request.result[options.validationKey] ;
 						options.callback(1);
 					}
@@ -85,11 +84,11 @@ function getIndexedDBObject(name) {
 				if ( transaction ) {
 					var objectStore = transaction.objectStore(OBJECTSTORENAME) ;
 
-                	var index = objectStore.index(options.columnName);  
-                	index.get(options.record[options.columnName]).onsuccess = function(event) {  
+                	var index = objectStore.index(options.columnName);
+                	index.get(options.record[options.columnName]).onsuccess = function(event) {
                 			event.target.result[options.validationKey] ;
                 			options.callback(1);
-                	}; 
+                	};
 				}
 				else
 					options.callback(0);
@@ -104,7 +103,7 @@ function getIndexedDBObject(name) {
 				if ( transaction ) {
 					var objectStore = transaction.objectStore(OBJECTSTORENAME) ;
 
-					var boundKeyRange = getIndexedDBObject('IDBKeyRange').bound(options.lowerBound[options.columnName], options.upperBound[options.columnName], false, false);  
+					var boundKeyRange = getIndexedDBObject('IDBKeyRange').bound(options.lowerBound[options.columnName], options.upperBound[options.columnName], false, false);
 					var count = 0 ;
 					objectStore.openCursor(boundKeyRange).onsuccess = function(event) {
 						var cursor = event.target.result;
@@ -134,14 +133,14 @@ function getIndexedDBObject(name) {
 				var transaction = getTransaction('readonly') ;
 				if ( transaction ) {
 					var objectStore = transaction.objectStore(OBJECTSTORENAME) ;
-	
+
             		var index = objectStore.index(options.columnName);
             		var boundKeyRange = getIndexedDBObject('IDBKeyRange').bound(options.lowerBound[options.columnName], options.upperBound[options.columnName], false, false);
             		var count = 0 ;
             		index.openCursor(boundKeyRange).onsuccess = function(event) {
             			var cursor = event.target.result;
                 		if (cursor && (!options.limit || count < options.limit)) {
-                			cursor.value[options.validationKey] ; 
+                			cursor.value[options.validationKey] ;
                 			count++ ;
                				cursor.continue();
                 		}
@@ -168,23 +167,23 @@ function getIndexedDBObject(name) {
 
 
 	function openIDB(options) {
-		
+
 		dbconn = idb.open( DBNAME, VERSION ) ;
 
-		dbconn.onerror = function(event) {  
+		dbconn.onerror = function(event) {
 			window.test.log.error('Could not setup an IndexedDB database (' + dbconn.errorCode + ')') ;
 			disabled = true ;
 			options.callback(0) ;
-		};  
+		};
 		dbconn.onsuccess = function(event) {
-			// cleanup	
+			// cleanup
 			var db = event.target.result ;
 			try {
 				var transaction = getTransaction('readwrite') ;
 				if ( transaction ){
 					if ( !options.skip) {
-						if ( transaction.db.objectStoreNames.contains(OBJECTSTORENAME) ) {	
-							var objectStore = transaction.objectStore(OBJECTSTORENAME);  
+						if ( transaction.db.objectStoreNames.contains(OBJECTSTORENAME) ) {
+							var objectStore = transaction.objectStore(OBJECTSTORENAME);
 							objectStore.clear() ;
 							options.callback(1) ;
 						}
@@ -208,24 +207,24 @@ function getIndexedDBObject(name) {
 				options.callback(0) ;
 			}
 		} ;
-		dbconn.onupgradeneeded = function(event) {  
-			// Update object stores and indices    
+		dbconn.onupgradeneeded = function(event) {
+			// Update object stores and indices
 			console.info('onupgradeneeded') ;
 
 			var db = event.target.result;
-            
+
             while (db.objectStoreNames.length) {
               db.deleteObjectStore(db.objectStoreNames[0]);
             }
 
 			var objectStore = db.createObjectStore( OBJECTSTORENAME, {keyPath:"ssn"} ) ;
-			objectStore.createIndex("name","name",{unique:false});         
+			objectStore.createIndex("name","name",{unique:false});
 			objectStore.createIndex("email","email",{unique:true});
 		} ;
 	}
 
 	function getIndexedDBObject(name) {
-		return window[name[0].toLowerCase() + name.slice(1)] 
+		return window[name[0].toLowerCase() + name.slice(1)]
             || window['webkit' + name] || window['moz' + name] || window['ms' + name] ;
 	}
 	function getTransaction( mode) {
